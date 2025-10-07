09e8292af23da0956099d1dafd886aba78879555
Thiago Oliveira <geovanithiago08@gmail.com>
2025-10-06 23:44:24 -0300

    Adição de gerador de PDF

--- script.js
+++ script.js
@@ -18,11 +18,12 @@ const btnGenerate = document.querySelector("#generate-pdf");
 btnGenerate.addEventListener("click", () => {
     // Conteúdo do PDF
     const preview  = document.querySelector("#preview")
+    // const pagamento = documento.querySelector("#pagamento")
 
     // Configuração do arquivo final PDF
     const options = {
         margin: [10, 10 , 10, 10],
-        filename: "arquivo.pdf",
+        filename: "Pedido.pdf",
         html2canvas: {scale: 2},
         jsPDF: {unit: "mm", format: "a4", orientation: "portrait" },
 
